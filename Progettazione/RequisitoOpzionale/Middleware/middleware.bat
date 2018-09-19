7a4e0e7109861435663dd82f7bef1e162ae8cff8
AndywinXp <andywinxp@gmail.com>
2018-09-19 20:34:01 +0200

    Modificati middleware e qa per una chiusura soft a fine pulizia

--- Progettazione/RequisitoOpzionale/Middleware/middleware.bat
+++
@@ -1,3 +0,0 @@
-cd "C:\Users\Andrea\Desktop\Universit…\INGSW\PROGETTO 240818\Middleware"
-node middleware.js -l
-
--- Progettazione/SoffrittiServer/WebGLScene/js/eventBus/events.js
+++
@@ -1,4 +0,0 @@
-export default {
-    sonarActivated: 'sonarActivated',
-    collision: 'collision'    
-}
